842e132d65af0bc9ea669e9f42acdf327c3a2547
Ahmad Alqadhy <ahmadalqadhy@gmail.com>
2020-07-05 18:06:16 -0700

    final not working

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 const express = require("express")
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
 const app = express()
 const db = require("./config/keys").mongoURI;
+const passport = require("passport");
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 app.get("/", (req, res) => res.send("Hello bitches"));
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
